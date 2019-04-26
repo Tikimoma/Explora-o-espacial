ec9ad7feaf549ce71dae7465a7b4c74d0509cfd5
Tikimoma <ti.gabrielmartins@gmail.com>
2019-04-25 23:26:18 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -4,18 +4,14 @@
 
 let todosBotoes = document.getElementsByClassName('botao-expandir-retrair');
 
-let botoes = document.querySelectorAll('.botao-expandir-retrair');
-
   for (let botao of todosBotoes) {
       botao.addEventListener('click', function (e) {
           if (botao.parentNode.classList.contains('expandido')) {
             botao.innerHTML = "+";
             botao.parentNode.classList.remove('expandido');
-
           } else {
               botao.innerHTML = "-";
               botao.parentNode.classList.add('expandido');
-
           }
       });
   }
